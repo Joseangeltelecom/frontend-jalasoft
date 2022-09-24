e5c3af116ded7e4b884e3f151cba92d913d554a3
jose angel <joseangeltelecom@gmail.com>
2022-09-24 11:59:50 -0500

    Adding length

--- task1/task1.js
+++ task1/task1.js
@@ -4,7 +4,7 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 function average1(numbers){
   const sum = numbers.reduce((acc, curr) => acc + curr, 0);
-  return sum / length
+  return sum / numbers.length
 }
 
 const average2 = (numbers) => {
